2a62f32e3f078e61598a73e9d714cbdf4a33743c
skirubakaran <sudhakar.kirubakaran@williamhill.com>
2018-06-21 10:27:40 +0100

    Refactored code with updated method names

--- testra-api-client/src/main/java/tech/testra/jvm/api/client/api/TestraRestClient.java
+++ testra-api-client/src/main/java/tech/testra/jvm/api/client/api/TestraRestClient.java
@@ -1,11 +1,5 @@
 package tech.testra.jvm.api.client.api;
 
-
-import static tech.testra.jvm.api.util.PropertyHelper.prop;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.testra.jvm.client.ApiException;
@@ -13,19 +7,18 @@ import tech.testra.jvm.client.api.ExecutionApi;
 import tech.testra.jvm.client.api.ProjectApi;
 import tech.testra.jvm.client.api.ResultApi;
 import tech.testra.jvm.client.api.ScenarioApi;
-import tech.testra.jvm.client.model.Execution;
-import tech.testra.jvm.client.model.ExecutionRequest;
-import tech.testra.jvm.client.model.Project;
-import tech.testra.jvm.client.model.ProjectRequest;
-import tech.testra.jvm.client.model.Scenario;
-import tech.testra.jvm.client.model.ScenarioRequest;
-import tech.testra.jvm.client.model.TestResult;
-import tech.testra.jvm.client.model.TestResultRequest;
+import tech.testra.jvm.client.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static tech.testra.jvm.api.util.PropertyHelper.prop;
 
 public final class TestraRestClient {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TestraRestClient.class);
-  private static final String TESTRA_HOST= prop("host");
+    private static final String TESTRA_HOST = prop("host");
   private static final String PROJECTID = "projectID";
   private static final String EXECUTIONID = "executionID";
   private static String projectIDString;
@@ -35,41 +28,42 @@ public final class TestraRestClient {
   private ExecutionApi executionApi = new ExecutionApi();
   private ResultApi resultApi = new ResultApi();
 
-
-
-  public TestraRestClient(){
+    public TestraRestClient() {
     projectApi.getApiClient().setDebugging(true);
   }
 
-
-  public void setURLs(String url){
+    public void setURLs(String url) {
     projectApi.getApiClient().setBasePath(url);
     executionApi.getApiClient().setBasePath(url);
     resultApi.getApiClient().setBasePath(url);
     scenarioApi.getApiClient().setBasePath(url);
   }
 
-  public String getProjectID(String projectName){
+    public String getProjectID(String projectName) {
     List<Project> projects = new ArrayList<>();
     try {
-      projects = projectApi.projectsGet();
+        projects = projectApi.getProjects();
     } catch (ApiException e) {
       e.printStackTrace();
     }
-    if(projects.stream().anyMatch(x -> x.getName().equals(projectName))){
-      projectIDString = projects.stream().filter(x -> x.getName().equals(projectName)).collect(
-          Collectors.toList()).get(0).getId();
+        if (projects.stream().anyMatch(x -> x.getName().equals(projectName))) {
+            projectIDString =
+                    projects
+                            .stream()
+                            .filter(x -> x.getName().equals(projectName))
+                            .collect(Collectors.toList())
+                            .get(0)
+                            .getId();
       return projectIDString;
-    }
-    else{
+        } else {
       LOGGER.error("No project found with name " + projectName);
       throw new IllegalArgumentException("Unknown project");
     }
   }
 
-  public String createScenario(ScenarioRequest scenarioRequest){
+    public String createScenario(ScenarioRequest scenarioRequest) {
     try {
-      Scenario scenario = scenarioApi.projectsProjectIdScenariosPost(projectIDString, scenarioRequest);
+        Scenario scenario = scenarioApi.createScenario(projectIDString, scenarioRequest);
       return scenario.getId();
     } catch (ApiException e) {
       LOGGER.error("Error Creating Scenario " + scenarioRequest.getName());
@@ -79,11 +73,11 @@ public final class TestraRestClient {
     }
   }
 
-  public String createExecution(){
+    public String createExecution() {
     ExecutionRequest executionRequest = new ExecutionRequest();
     executionRequest.setIsParallel(false);
     try {
-      Execution execution = executionApi.projectsProjectIdExecutionsPost(projectIDString, executionRequest);
+        Execution execution = executionApi.createExecution(projectIDString, executionRequest);
       executionIDString = execution.getId();
       return executionIDString;
     } catch (ApiException e) {
@@ -94,14 +88,13 @@ public final class TestraRestClient {
     }
   }
 
-  public void createResult(TestResultRequest testResultRequest){
+    public void createResult(TestResultRequest testResultRequest) {
     try {
-      resultApi.projectsProjectIdExecutionsExecutionIdResultsPost(projectIDString, executionIDString, testResultRequest);
+        resultApi.createResult(projectIDString, executionIDString, testResultRequest);
     } catch (ApiException e) {
       LOGGER.error("Error Creating Result " + testResultRequest.getTargetId());
       LOGGER.error(e.getResponseBody());
       e.printStackTrace();
     }
   }
-
 }
